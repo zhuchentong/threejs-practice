138773344a99de68aabf3f0819c0ecfd583767eb
zhuchentong <2037630@163.com>
2022-04-07 18:25:46 +0800

    WIP

---
+++ src/components/hello-threejs/index.tsx
@@ -0,0 +1,83 @@
+import React, { useEffect, useRef } from "react";
+import {
+  BoxGeometry,
+  DirectionalLight,
+  Mesh,
+  MeshPhongMaterial,
+  PerspectiveCamera,
+  Scene,
+  WebGLRenderer,
+} from "three";
+import "./index.scss";
+
+const HelloThreejs: React.FC = () => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    if (canvasRef.current) {
+      // 创建渲染器
+      const renderer = new WebGLRenderer({ canvas: canvasRef.current });
+
+      // 创建镜头
+      const camera = new PerspectiveCamera(75, 2, 0.1, 5);
+
+      // 创建场景
+      const scene = new Scene();
+
+      //创建几何体
+      const geometry = new BoxGeometry(1, 1, 1);
+
+      // 创建材质
+      const material1 = new MeshPhongMaterial({ color: 0x44aa88 });
+      const material2 = new MeshPhongMaterial({ color: 0xc50d0d });
+      const material3 = new MeshPhongMaterial({ color: 0x39b20a });
+
+      // 创建网格
+      const cube1 = new Mesh(geometry, material1);
+      cube1.position.x = -2;
+      scene.add(cube1);
+
+      const cube2 = new Mesh(geometry, material2);
+      cube2.position.x = -0;
+      scene.add(cube2);
+
+      const cube3 = new Mesh(geometry, material3);
+      cube3.position.x = 2;
+      scene.add(cube3);
+
+      const cubes = [cube1, cube2, cube3];
+      // 创建光源
+      const light = new DirectionalLight(0xffffff, 1);
+
+      // 设置光源位置
+      light.position.set(-1, 2, 4);
+      scene.add(light);
+
+      camera.position.z = 2;
+
+      const render = (time: number) => {
+        time = time * 0.001;
+
+        // 修改形变比例
+        const canvas = renderer.domElement;
+        camera.aspect = canvas.clientWidth / canvas.clientHeight;
+        camera.updateProjectionMatrix();
+
+        renderer.setSize(canvas.clientWidth, canvas.clientHeight, false);
+
+        cubes.forEach((cube) => {
+          cube.rotation.x = time;
+          cube.rotation.y = time;
+        });
+
+        renderer.render(scene, camera);
+        window.requestAnimationFrame(render);
+      };
+      window.requestAnimationFrame(render);
+    }
+  }, [canvasRef]);
+
+  return <canvas className="full-screen" ref={canvasRef}></canvas>;
+};
+
+export default HelloThreejs;
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createRoot } from "react-dom/client";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import "./index.scss";
+import App from "./App";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const container = document.getElementById("root");
+if (container) {
+  const root = createRoot(container);
+  root.render(<App></App>);
+}
